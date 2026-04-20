3c4713905607088b7216325bb8d3baaee0f04f69
Suhani kumari <suhanidhanupur@.com>
2026-04-20 23:33:35 +0530

    added

--- crud_project/crud_app/models.py
+++ crud_project/crud_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    std_name = models.CharField(max_length=100)
+    std_roll = models.IntegerField()
+    std_image = models.ImageField(upload_to='students/')
+    std_city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.std_name
